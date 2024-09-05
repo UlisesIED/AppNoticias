5f88fb260f35676f553b63d4411855afdd847817
UlisesIED <uliescodi@gmail.com>
2024-09-05 11:22:21 -0600

    build: Librerias necesarias, navigator e icons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import type { PropsWithChildren } from 'react';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,6 +9,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
   Colors,
@@ -29,7 +23,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -79,6 +73,7 @@ function App(): React.JSX.Element {
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
+            <Icon name="umbrella-sharp" size={30} color="#4F8EF7" />
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
